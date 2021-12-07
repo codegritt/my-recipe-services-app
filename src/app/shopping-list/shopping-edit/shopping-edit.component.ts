a956e0e4ef12b002189a308bb1cf8b9006494c81
Gokul <gokulchandan18@gmail.com>
2021-12-07 22:34:29 +0530

    commit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -3,11 +3,10 @@ import {
   OnInit,
   ElementRef,
   ViewChild,
-  EventEmitter,
-  Output
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -19,9 +18,9 @@ export class ShoppingEditComponent implements OnInit {
   nameInputRef!: ElementRef;
   @ViewChild('amountInput', { static: false })
   amountInputRef!: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+ 
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -30,7 +29,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+  this.slService.addIngredient(newIngredient);
   }
 
 }
